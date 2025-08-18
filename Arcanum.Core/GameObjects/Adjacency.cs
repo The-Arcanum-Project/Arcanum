227e60119bed5ab8b78c1724a1df07140bea5dc9
Minnator <minnator.info@web.de>
2025-08-19 00:54:49 +0200

    Add `DiagnosticArgsAnalyzer` and unit tests
    - Introduced `DiagnosticArgsAnalyzer` for validating diagnostic arguments in Roslyn-based analyzers.
    - Added `DiagnosticArgsAnalyzerTest` to ensure correct behavior for varying argument configurations.
    - Updated `Arcanum.Core` and solution files to include the new analyzer.
    - Enhanced UnitTests project with required dependencies for analyzer testing.

---
+++ Arcanum.Core/GameObjects/Adjacency.cs
@@ -0,0 +1,49 @@
+﻿using Arcanum.Core.GameObjects.LocationCollections;
+
+namespace Arcanum.Core.GameObjects;
+
+public enum AdjacencyType
+{
+   Sea,
+}
+
+public class Adjacency
+{
+   public Adjacency(Location from,
+                    Location to,
+                    AdjacencyType type,
+                    string name,
+                    string comment,
+                    int startX,
+                    int startY,
+                    int endX,
+                    int endY)
+   {
+      From = from;
+      To = to;
+      Type = type;
+      Name = name;
+      Comment = comment;
+      StartX = startX;
+      StartY = startY;
+      EndX = endX;
+      EndY = endY;
+   }
+
+   public Location From { get; }
+   public Location To { get; }
+   public AdjacencyType Type { get; }
+   public string Name { get; }
+   public string Comment { get; }
+   public int StartX { get; }
+   public int StartY { get; }
+   public int EndX { get; }
+   public int EndY { get; }
+
+   public override string ToString() => $"{Name,15}: {From.Name,15} -> {To.Name,15} ({Type})";
+
+   public override bool Equals(object? obj)
+      => obj is Adjacency other && other.Name.Equals(Name, StringComparison.Ordinal);
+
+   public override int GetHashCode() => Name.GetHashCode();
+}
