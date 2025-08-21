08ea84afee2461ad6f3134a3594c9769a83f81fd
Minnator <minnator.info@web.de>
2025-08-22 00:49:50 +0200

    Refactor NUI view generation and embedded views:
    - Replaced `BaseEmbeddedBorder` with `ContentGrid` and updated `EmbedMarker` in `BaseEmbeddedView`.
    - Enhanced `NUIViewGenerator` to support dynamic grid layouts and collection-based UI rendering.
    - Introduced `BuildCollectionOrDefaultView` for improved handling of collections and arrays in the UI.
    - Added utility methods for type determination (`GetCollectionItemType` and `GetArrayItemType`).
    - Improved overall layout consistency, styling, and margin handling across components.

--- Arcanum.UI/NUI/Generator/NUIViewGenerator.cs
+++ Arcanum.UI/NUI/Generator/NUIViewGenerator.cs
@@ -2,6 +2,9 @@
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using Arcanum.Core.CoreSystems.NUI;
 using Arcanum.UI.Components.StyleClasses;
 using Arcanum.UI.NUI.Headers;
@@ -13,7 +16,7 @@ namespace Arcanum.UI.NUI.Generator;
 
 public static class NUIViewGenerator
 {
-   private static int index = 0;
+   private static int _index;
 
    public static void GenerateAndSetView(NUINavHistory navHistory)
    {
@@ -27,9 +30,12 @@ public static class NUIViewGenerator
       var titleBinding = GetOneWayBinding(target, target.Settings.Title);
       var subtitleBinding = GetOneWayBinding(target, target.Settings.Description);
 
-      var baseUI = new BaseView { Name = $"{target.Settings.Title}_{index}" };
+      var baseUI = new BaseView
+      {
+         Name = $"{target.Settings.Title}_{_index}", BaseViewBorder = { BorderThickness = new(0) },
+      };
 
-      var baseGrid = new Grid { RowDefinitions = { new() { Height = new(50, GridUnitType.Pixel) } } };
+      var baseGrid = new Grid { RowDefinitions = { new() { Height = new(40, GridUnitType.Pixel) } }, Margin = new(4) };
 
       var header = GetDescHeader(titleBinding, subtitleBinding, target.Navigations, navHistory.Root, target);
       baseGrid.Children.Add(header);
@@ -58,8 +64,7 @@ public static class NUIViewGenerator
          }
          else
          {
-            element = GetStackPanel(target, nxProp);
-            baseGrid.RowDefinitions.Add(new() { Height = new(25, GridUnitType.Pixel) });
+            element = BuildCollectionOrDefaultView(navHistory.Root, type, target, nxProp, baseGrid);
          }
 
          baseGrid.Children.Add(element);
@@ -68,21 +73,23 @@ public static class NUIViewGenerator
       }
 
       baseUI.BaseViewBorder.Child = baseGrid;
-      index++;
+      _index++;
       return baseUI;
    }
 
-   public static UserControl GetEmbeddedView<T>(T target,
-                                                ContentPresenter root) where T : INUI
+   private static BaseEmbeddedView GetEmbeddedView<T>(T target,
+                                                      ContentPresenter root) where T : INUI
    {
       var header = target.Settings.Title;
       var embeddedFields = target.Settings.EmbeddedFields;
 
       var baseUI = new BaseEmbeddedView();
 
-      var baseGrid = new Grid { RowDefinitions = { new() { Height = new(25, GridUnitType.Pixel) } } };
+      var baseGrid = baseUI.ContentGrid; 
+      baseGrid.RowDefinitions.Add(new() { Height = new(30, GridUnitType.Pixel) });
       var titleBinding = GetOneWayBinding(target, header);
       var headerBlock = NavigationHeader(titleBinding, target.Navigations, root, target);
+      headerBlock.Margin = new(6, 0, 0, 0);
       Grid.SetRow(headerBlock, 0);
       Grid.SetColumn(headerBlock, 0);
       baseGrid.Children.Add(headerBlock);
@@ -101,8 +108,7 @@ public static class NUIViewGenerator
          }
          else
          {
-            element = GetStackPanel(target, nxProp);
-            baseGrid.RowDefinitions.Add(new() { Height = new(25, GridUnitType.Pixel) });
+            element = BuildCollectionOrDefaultView(root, type, target, nxProp, baseGrid, 6);
          }
 
          baseGrid.Children.Add(element);
@@ -110,7 +116,8 @@ public static class NUIViewGenerator
          Grid.SetColumn(element, 0);
       }
 
-      baseUI.BaseEmbeddedBorder.Child = baseGrid;
+      Grid.SetRowSpan(baseUI.EmbedMarker, baseGrid.RowDefinitions.Count);
+      
       return baseUI;
    }
 
@@ -119,20 +126,36 @@ public static class NUIViewGenerator
       var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
       object headerValue = null!;
       Nx.ForceGet(value, value.Settings.Title, ref headerValue);
-      var headerBlock = new TextBlock
-      {
-         Text = headerValue.ToString() ?? "Unknown", Cursor = Cursors.Hand,
-      };
       var sInfo = string.Empty;
       foreach (var nxProp in value.Settings.ShortInfoFields)
       {
          if (sInfo.Length > 0)
             sInfo += ", ";
-         object propValue = null!;
-         Nx.ForceGet(value, nxProp, ref propValue);
-         sInfo += $"{propValue}";
+
+         var type = Nx.TypeOf(value, nxProp);
+         var itemType = GetCollectionItemType(type);
+         var arrayType = GetArrayItemType(type);
+         if (itemType != null && typeof(INUI).IsAssignableFrom(itemType))
+         {
+            ICollection<INUI> collection = null!;
+            Nx.ForceGet(value, nxProp, ref collection);
+            sInfo += $"{collection.Count}:{nxProp}";
+         }
+         else if (arrayType != null && typeof(INUI).IsAssignableFrom(arrayType))
+         {
+            INUI[] array = null!;
+            Nx.ForceGet(value, nxProp, ref array);
+            sInfo += $"{array.Length}:{nxProp}";
+         }
+         else
+         {
+            object propValue = null!;
+            Nx.ForceGet(value, nxProp, ref propValue);
+            sInfo += $"{propValue}";
+         }
       }
 
+      var headerBlock = NavigationHeader(GetOneWayBinding(value, value.Settings.Title), value.Navigations, root, value);
       var infoBlock = new TextBlock
       {
          Text = sInfo, Cursor = Cursors.Hand,
@@ -140,47 +163,79 @@ public static class NUIViewGenerator
       stackPanel.Children.Add(headerBlock);
       stackPanel.Children.Add(infoBlock);
 
-      headerBlock.MouseUp += (sender, e) =>
-      {
-         if (e.ChangedButton == MouseButton.Right)
-         {
-            if (value.Navigations.Length == 0)
-            {
-               e.Handled = true;
-               return;
-            }
-
-            var contextMenu = GetContextMenu(value.Navigations, root, value);
-            contextMenu.PlacementTarget = sender as UIElement ?? headerBlock;
-            contextMenu.IsOpen = true;
-            e.Handled = true;
-         }
-         else if (e.ChangedButton == MouseButton.Left)
-         {
-            root.Content = GenerateView(new(value, true, root));
-         }
-      };
       return stackPanel;
    }
 
-   private static StackPanel GetStackPanel(INUI target, Enum nxProp)
+   private static Grid GetDefaultGrid(INUI target, Enum nxProp, int leftMargin = 0)
    {
+      UIElement element;
+      var desc = DescriptorBlock(nxProp);
+      desc.Margin = new (leftMargin, 0, 0, 0);
       switch (Nx.TypeOf(target, nxProp))
       {
          case var t when t == typeof(string):
          case var f when f == typeof(float):
-            var desc = DescriptorBlock(nxProp);
-            var textBox = new CorneredTextBox();
+            var textBox = new CorneredTextBox
+            {
+               Height = 23,
+               FontSize = 12,
+               Margin = new(0),
+               BorderThickness = new(1),
+            };
             textBox.SetBinding(TextBox.TextProperty, GetTwoWayBinding(target, nxProp));
-            return new() { Children = { desc, textBox }, Orientation = Orientation.Horizontal };
+            element = textBox;
+            break;
+         default:
+            throw
+               new NotSupportedException($"Type {Nx.TypeOf(target, nxProp)} is not supported for property {nxProp}.");
       }
+      
+      
+      
+      var line = new Rectangle
+      {
+         Height = 1,
+         Fill = Brushes.Transparent,
+         Stroke = (Brush)Application.Current.FindResource("SelectedBackColorBrush")!,
+         StrokeThickness = 1,
+         StrokeDashArray = new([4, 6]),
+         StrokeDashCap = PenLineCap.Flat,
+         HorizontalAlignment = HorizontalAlignment.Stretch,
+         VerticalAlignment = VerticalAlignment.Bottom,
+         SnapsToDevicePixels = true,
+         Margin = new (leftMargin, 0, 5, 3),
+      };
+      RenderOptions.SetEdgeMode(line, EdgeMode.Aliased);
 
-      throw new NotSupportedException($"Type {Nx.TypeOf(target, nxProp)} is not supported for property {nxProp}.");
+      var grid = new Grid
+      {
+         HorizontalAlignment = HorizontalAlignment.Stretch,
+         VerticalAlignment = VerticalAlignment.Top,
+         RowDefinitions = { new() { Height = new(25, GridUnitType.Pixel) } },
+         ColumnDefinitions =
+         {
+            new() { Width = new(4, GridUnitType.Star) }, new() { Width = new(6, GridUnitType.Star) },
+         },
+      };
+
+      grid.Children.Add(desc);
+      Grid.SetRow(desc, 0);
+      Grid.SetColumn(desc, 0);
+      
+      grid.Children.Add(line);
+      Grid.SetRow(line, 0);
+      Grid.SetColumn(line, 0);
+
+      grid.Children.Add(element);
+      Grid.SetRow(element, 0);
+      Grid.SetColumn(element, 1);
+
+      return grid;
    }
 
    private static TextBlock DescriptorBlock(Enum nxProp)
    {
-      var textBlock = new TextBlock { Text = $"{nxProp}: " };
+      var textBlock = new TextBlock { Text = $"{nxProp}: ", VerticalAlignment = VerticalAlignment.Center };
       return textBlock;
    }
 
@@ -202,7 +257,7 @@ public static class NUIViewGenerator
                return;
             }
 
-            var contextMenu = GetContextMenu(navigations, root, value);
+            var contextMenu = GetContextMenu(navigations, root);
             contextMenu.PlacementTarget = sender as UIElement ?? header;
             contextMenu.IsOpen = true;
             e.Handled = true;
@@ -217,8 +272,7 @@ public static class NUIViewGenerator
       return header;
    }
 
-   private static ContextMenu GetContextMenu<T>(INUINavigation[] navigations, ContentPresenter root, T target)
-      where T : INUI
+   private static ContextMenu GetContextMenu(INUINavigation[] navigations, ContentPresenter root)
    {
       var contextMenu = new ContextMenu();
       foreach (var navigation in navigations)
@@ -250,7 +304,7 @@ public static class NUIViewGenerator
                return;
             }
 
-            var contextMenu = GetContextMenu(navigations, root, target);
+            var contextMenu = GetContextMenu(navigations, root);
             contextMenu.PlacementTarget = sender as UIElement ?? header;
             contextMenu.IsOpen = true;
             e.Handled = true;
@@ -286,5 +340,141 @@ public static class NUIViewGenerator
          Mode = BindingMode.OneWay,
          UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
       };
+   }
+
+   public static Type? GetCollectionItemType(Type collectionType)
+   {
+      var enumerableInterface = collectionType.GetInterfaces()
+                                              .FirstOrDefault(i => i.IsGenericType &&
+                                                                   i.GetGenericTypeDefinition() ==
+                                                                   typeof(IEnumerable<>));
+
+      return enumerableInterface?.GetGenericArguments()[0];
+   }
+
+   public static Type? GetArrayItemType(Type arrayType)
+   {
+      return arrayType.IsArray ? arrayType.GetElementType() : null;
+   }
+
+   private static BaseButton GetEyeButton()
+   {
+      return new()
+      {
+         Margin = new(2),
+         Height = 20,
+         Width = 20,
+         Content = new Image()
+         {
+            Source = new BitmapImage(new("pack://application:,,,/Assets/Icons/20x20/Eye20x20.png")),
+            Stretch = Stretch.None,
+         }
+      };
+   }
+
+   private static UIElement BuildCollectionOrDefaultView(ContentPresenter root,
+                                                         Type type,
+                                                         INUI target,
+                                                         Enum nxProp,
+                                                         Grid baseGrid,
+                                                         int leftmargin = 0)
+   {
+      UIElement element;
+      var itemType = GetCollectionItemType(type);
+      var arrayType = GetArrayItemType(type);
+
+      if (itemType == null || arrayType == null)
+      {
+         // 6 rows, 2 columns
+         var grid = new Grid
+         {
+            RowDefinitions =
+            {
+               new() { Height = new(25, GridUnitType.Pixel) },
+               new() { Height = new(20, GridUnitType.Pixel) },
+               new() { Height = new(20, GridUnitType.Pixel) },
+               new() { Height = new(20, GridUnitType.Pixel) },
+               new() { Height = new(20, GridUnitType.Pixel) },
+            },
+            ColumnDefinitions =
+            {
+               new() { Width = new(1, GridUnitType.Star) }, new() { Width = new(1, GridUnitType.Star) },
+            },
+            Margin = new(leftmargin, 0, 0, 0),
+         };
+
+         var count = -1;
+
+         // Collection of INUI
+         if (typeof(INUI).IsAssignableFrom(itemType))
+         {
+            // We show the first 5 items of the collection as a ShortInfo
+            ICollection<INUI> collection = null!;
+            Nx.ForceGet(target, nxProp, ref collection);
+
+            foreach (var j in Enumerable.Range(0, Math.Min(5, collection.Count)))
+            {
+               var value = collection.ElementAt(j);
+               var shortInfo = GenerateShortInfo(value, root);
+               grid.Children.Add(shortInfo);
+               Grid.SetRow(shortInfo, j);
+               Grid.SetColumn(shortInfo, 0);
+               Grid.SetColumnSpan(shortInfo, 2);
+            }
+
+            count = collection.Count;
+            element = grid;
+         }
+         else if (typeof(INUI).IsAssignableFrom(arrayType))
+         {
+            // We show the first 5 items of the array as a ShortInfo
+            INUI[] array = null!;
+            Nx.ForceGet(target, nxProp, ref array);
+
+            foreach (var j in Enumerable.Range(0, Math.Min(5, array.Length)))
+            {
+               var value = array[j];
+               var shortInfo = GenerateShortInfo(value, root);
+               grid.Children.Add(shortInfo);
+               Grid.SetRow(shortInfo, j);
+               Grid.SetColumn(shortInfo, 0);
+               Grid.SetColumnSpan(shortInfo, 2);
+            }
+
+            count = array.Length;
+            element = grid;
+         }
+         else
+         {
+            element = GetDefaultGrid(target, nxProp, leftmargin);
+            baseGrid.RowDefinitions.Add(new() { Height = new(25, GridUnitType.Pixel) });
+         }
+
+         var nheader = new TextBlock
+         {
+            Text = $"{nxProp}: {count} Items", FontWeight = FontWeights.Bold,
+         };
+         grid.Children.Add(nheader);
+         Grid.SetRow(nheader, 0);
+         Grid.SetColumn(nheader, 0);
+
+         var openButton = GetEyeButton();
+
+         openButton.Click += (sender, e) =>
+         {
+            // TODO open proper collection editor for any kind of object
+         };
+
+         grid.Children.Add(openButton);
+         Grid.SetRow(openButton, 0);
+         Grid.SetColumn(openButton, 1);
+      }
+      else
+      {
+         element = GetDefaultGrid(target, nxProp, leftmargin);
+         baseGrid.RowDefinitions.Add(new() { Height = new(25, GridUnitType.Pixel) });
+      }
+
+      return element;
    }
 }
