cb866604929198e6b26e9437ba277a63df08a7a8
mel-co <mel-co@users.noreply.github.com>
2025-09-26 22:09:45 +0200

    Refactor GridSplitter and ToggleSwitch styles; introduce HalfRadiusConverter for improved corner radius handling

--- Arcanum.UI/Components/Converters/HalfHeightRadiusConverter.cs
+++ Arcanum.UI/Components/Converters/HalfHeightRadiusConverter.cs
@@ -4,28 +4,14 @@ using System.Windows.Data;
 
 namespace Arcanum.UI.Components.Converters;
 
-public class HeightToRadiusConverter : IMultiValueConverter
+public class HalfRadiusConverter : IValueConverter
 {
-    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    public object Convert(object values, Type targetType, object parameter, CultureInfo culture)
     {
-        if (values.Length <= 0 || values[0] is not double height || !(height > 0)) return DependencyProperty.UnsetValue;
-        // For CornerRadius
-        if (targetType == typeof(CornerRadius))
-        {
-            return new CornerRadius(height / 2);
-        }
-                
-        // For Thumb Size (Width/Height)
-        if (targetType == typeof(double))
-        {
-            return height - 4; // 2px padding on each side
-        }
-
-        // For Thumb Margin
-        return targetType == typeof(Thickness) ? new Thickness(2,0,0,0) : DependencyProperty.UnsetValue;
+        return values is not (double height and > 0) ? DependencyProperty.UnsetValue : new CornerRadius(height / 2);
     }
 
-    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
     }
