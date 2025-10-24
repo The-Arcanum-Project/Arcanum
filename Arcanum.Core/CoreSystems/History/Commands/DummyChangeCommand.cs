602da3c8737bc51aad2c1aa0c64e07d5cfcfe11c
Minnator <minnator.info@web.de>
2025-10-25 00:13:12 +0200

    da

--- Arcanum.Core/CoreSystems/History/Commands/DummyChangeCommand.cs
+++ Arcanum.Core/CoreSystems/History/Commands/DummyChangeCommand.cs
@@ -5,61 +5,63 @@ namespace Arcanum.Core.CoreSystems.History.Commands;
 
 public class DummyChangeCommand : ICommand
 {
-    
-    private IEu5Object[] _targets = [];
-    private List<IEu5Object> _targetsInitial = [];
-    private readonly Enum _attribute;
-    private readonly Type _type;
-    
-    public string GetDescription => $"DummyChangeCommand targeting {_targets.Length} objects with attribute {_attribute}";
-    
-    public DummyChangeCommand(IEu5Object target, Enum attribute)
-    {
-        _type = target.GetType();
-        _targetsInitial.Add(target);
-        _attribute = attribute;
-    }
+   private IEu5Object[] _targets = [];
+   private List<IEu5Object> _targetsInitial = [];
+   private readonly Enum _attribute;
+   private readonly Type _type;
 
-    public void FinalizeSetup()
-    {
-        _targets = _targetsInitial.ToArray();
-        _targetsInitial = null!;
-    }
+   public string GetDescription => _targetsInitial == null!
+                                      ? $"DummyChangeCommand targeting {_targets.Length} objects with attribute {_attribute}"
+                                      : $"Waiting for more objects. Current: {_targetsInitial.Count} with attribute {_attribute}";
 
-    public void Execute()
-    {
-        SaveMaster.CommandExecuted(this);
-    }
+   public DummyChangeCommand(IEu5Object target, Enum attribute)
+   {
+      _type = target.GetType();
+      _targetsInitial.Add(target);
+      _attribute = attribute;
+   }
 
-    public void Undo()
-    {
-        SaveMaster.CommandUndone(this);
-    }
+   public void FinalizeSetup()
+   {
+      _targets = _targetsInitial.ToArray();
+      _targetsInitial = null!;
+   }
 
-    public void Redo()
-    {
-        SaveMaster.CommandExecuted(this);
-    }
+   public void Execute()
+   {
+      SaveMaster.CommandExecuted(this);
+   }
 
-    public List<int> GetTargetHash()
-    {
-        return _targets.Select(t => t.GetHashCode()).ToList();
-    }
+   public void Undo()
+   {
+      SaveMaster.CommandUndone(this);
+   }
 
-    public IEu5Object[] GetTargets() => _targets;
+   public void Redo()
+   {
+      SaveMaster.CommandExecuted(this);
+   }
 
-    public bool TryAdd(IEu5Object target, Enum attribute)
-    {
-        //TODO: @MelCo fix equality check for values
-        if(target.GetType() != _type || !attribute.Equals(_attribute))
-            return false;
-        _targetsInitial.Add(target);
-        return true;
-    }
+   public List<int> GetTargetHash()
+   {
+      return _targets.Select(t => t.GetHashCode()).ToList();
+   }
 
-    public string GetDebugInformation(int indent)
-    {
-        var indentStr = new string(' ', indent);
-        return $"{indentStr}DummyChangeCommand targeting {_targets.Length} objects.";
-    }
+   public IEu5Object[] GetTargets() => _targets;
+
+   public bool TryAdd(IEu5Object target, Enum attribute)
+   {
+      //TODO: @MelCo fix equality check for values
+      if (target.GetType() != _type || !attribute.Equals(_attribute))
+         return false;
+
+      _targetsInitial.Add(target);
+      return true;
+   }
+
+   public string GetDebugInformation(int indent)
+   {
+      var indentStr = new string(' ', indent);
+      return $"{indentStr}DummyChangeCommand targeting {_targets.Length} objects.";
+   }
 }
