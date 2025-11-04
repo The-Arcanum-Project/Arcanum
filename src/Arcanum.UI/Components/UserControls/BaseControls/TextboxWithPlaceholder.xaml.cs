d2b0f2ec8d0814b1d6cf87163d09d78f16b23dbb
zulacecek <rouca.jiri@seznam.cz>
2025-11-04 18:23:17 +0800

    Fix BaseControls/BaseNumericUpDown decrement button incrementing instead Hide "New Project" button when already in the Configure view Rename "Current configuration" button to "Create project" Add TextboxWithPlaceholder Use TextboxWithPlaceholder in Configuration tab path pickers

---
+++ src/Arcanum.UI/Components/UserControls/BaseControls/TextboxWithPlaceholder.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Arcanum.UI.Components.UserControls.BaseControls;
+
+public partial class TextboxWithPlaceholder : UserControl
+{
+    public static readonly DependencyProperty PlaceholderTextProperty =
+        DependencyProperty.Register(nameof(PlaceholderText), typeof(string), typeof(TextboxWithPlaceholder));
+
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
+    
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(nameof(Text), typeof(string), typeof(TextboxWithPlaceholder));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+
+    public TextboxWithPlaceholder()
+    {
+        InitializeComponent();
+    }
+}
