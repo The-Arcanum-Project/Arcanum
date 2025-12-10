133793a3c5955039a744f424bcb5bfd0bd7df3f3
Minnator <info@minnator.com>
2025-12-10 23:43:50 +0100

    Bitteschoen

--- src/Arcanum.Core/CoreSystems/Selection/SelectionHelpers.cs
+++ src/Arcanum.Core/CoreSystems/Selection/SelectionHelpers.cs
@@ -1,6 +1,8 @@
-﻿using Arcanum.Core.GameObjects.BaseTypes;
+﻿using System.Diagnostics;
+using Arcanum.Core.GameObjects.BaseTypes;
 using Arcanum.Core.GameObjects.LocationCollections;
 using Arcanum.Core.GameObjects.LocationCollections.BaseClasses;
+using Arcanum.Core.Registry;
 using Region = Arcanum.Core.GameObjects.LocationCollections.Region;
 
 namespace Arcanum.Core.CoreSystems.Selection;
@@ -31,6 +33,79 @@ public static class SelectionHelpers
       return current;
    }
 
+   public static IEu5Object GetParentOfType(IEu5Object current, Type targetType)
+   {
+      var curLevel = GetHierarchyLevel(current);
+      var targetLevel = GetHierarchyLevel((IEu5Object)EmptyRegistry.Empties[targetType]);
+
+      Debug.Assert(curLevel <= targetLevel);
+      if (curLevel == targetLevel)
+         return current;
+
+      var curObject = current;
+      while (curLevel < targetLevel)
+      {
+         curObject = GetNextBiggestParentObj(curObject);
+         curLevel++;
+         if (curLevel == targetLevel && curObject.GetType() == targetType)
+            return curObject;
+      }
+
+      throw new ArgumentException("No parent of the specified type found");
+   }
+
+   public static int GetHierarchyLevel(IEu5Object obj)
+   {
+      if (obj is Location)
+         return 0;
+      if (obj is Province)
+         return 1;
+      if (obj is Area)
+         return 2;
+      if (obj is Region)
+         return 3;
+      if (obj is SuperRegion)
+         return 4;
+      if (obj is Continent)
+         return 5;
+
+      throw new ArgumentException("obj is not a valid type in the hierarchy");
+   }
+
+   public static IEu5Object GetNextBiggestParentObj(IEu5Object current)
+   {
+      if (current is Location loc)
+         return loc.Province;
+      if (current is Province prov)
+         return prov.Area;
+      if (current is Area area)
+         return area.Region;
+      if (current is Region region)
+         return region.SuperRegion;
+      if (current is SuperRegion sRegion)
+         return sRegion.Continent;
+
+      throw new ArgumentException("current is not a valid parent type");
+   }
+
+   public static List<IEu5Object> GetAllChildren(IEu5Object eu5Object)
+   {
+      if (eu5Object is Location loc)
+         return [loc];
+      if (eu5Object is Province prov)
+         return prov.Locations.Cast<IEu5Object>().ToList();
+      if (eu5Object is Area area)
+         return area.Provinces.Cast<IEu5Object>().ToList();
+      if (eu5Object is Region region)
+         return region.Areas.Cast<IEu5Object>().ToList();
+      if (eu5Object is SuperRegion sRegion)
+         return sRegion.Regions.Cast<IEu5Object>().ToList();
+      if (eu5Object is Continent continent)
+         return continent.SuperRegions.Cast<IEu5Object>().ToList();
+
+      throw new ArgumentException("eu5Object is not a valid type in the hierarchy");
+   }
+
    public static ILocation? FindParentToShrinkTo(Location location)
    {
       var bp = FindBiggestFullySelectedParent(location);
@@ -42,7 +117,6 @@ public static class SelectionHelpers
    {
       return loc.LcType switch
       {
-         
          _ => null,
       };
    }
