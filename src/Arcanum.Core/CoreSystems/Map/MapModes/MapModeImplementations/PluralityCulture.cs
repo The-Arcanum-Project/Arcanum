2057a69fe97f424691c6a66bf70e40a41c263dc4
Minnator <info@minnator.com>
2025-12-14 22:02:49 +0100

    Add PluralityCulture and PluralityReligion map modes to display cultural and religious plurality

---
+++ src/Arcanum.Core/CoreSystems/Map/MapModes/MapModeImplementations/PluralityCulture.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using Arcanum.Core.GameObjects.Cultural;
+using Arcanum.Core.GameObjects.LocationCollections;
+
+namespace Arcanum.Core.CoreSystems.Map.MapModes.MapModeImplementations;
+
+public class PluralityCulture : LocationBasedMapMode
+{
+   public override string Name => "Plurality Culture";
+   public override string Description => "Displays the plurality culture of each location on the map.";
+   public override MapModeManager.MapModeType Type => MapModeManager.MapModeType.PluralityCulture;
+   public override Type[] DisplayTypes { get; } = [typeof(Culture)];
+
+   [ThreadStatic]
+   private static Dictionary<Culture, double>? _cultureMap;
+
+   public override int GetColorForLocation(Location location) => CalculatePluralityCulture(location).Color.AsInt();
+
+   private static Culture CalculatePluralityCulture(Location location)
+   {
+      _cultureMap ??= new(8);
+      _cultureMap.Clear();
+      foreach (var pop in location.Pops)
+      {
+         // Using CollectionsMarshal (Available in .NET 5+) avoids double-hashing (Contains + Add)
+         ref var currentWeight = ref CollectionsMarshal.GetValueRefOrAddDefault(_cultureMap, pop.Culture, out _);
+         currentWeight += pop.Size;
+      }
+
+      var bestCulture = Culture.Empty;
+      var maxWeight = -1.0;
+
+      foreach (var kvp in _cultureMap)
+         if (kvp.Value > maxWeight)
+         {
+            maxWeight = kvp.Value;
+            bestCulture = kvp.Key;
+         }
+
+      return bestCulture;
+   }
+
+   public override string[] GetTooltip(Location location)
+   {
+      var culture = CalculatePluralityCulture(location);
+      return [$"Plurality Culture: {culture.UniqueId}"];
+   }
+
+   public override string? GetLocationText(Location location) => null;
+
+   public override object?[]? GetVisualObject(Location location) => null;
+
+   public override void OnActivateMode()
+   {
+   }
+
+   public override void OnDeactivateMode()
+   {
+   }
+}
---
+++ src/Arcanum.Core/CoreSystems/Map/MapModes/MapModeImplementations/PluralityReligion.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using Arcanum.Core.GameObjects.LocationCollections;
+using Arcanum.Core.GameObjects.Religious;
+
+namespace Arcanum.Core.CoreSystems.Map.MapModes.MapModeImplementations;
+
+public class PluralityReligion : LocationBasedMapMode
+{
+   public override string Name => "Plurality Religion";
+   public override string Description => "Displays the plurality culture of each location on the map.";
+   public override MapModeManager.MapModeType Type => MapModeManager.MapModeType.PluralityReligion;
+   public override Type[] DisplayTypes { get; } = [typeof(Religion)];
+
+   [ThreadStatic]
+   private static Dictionary<Religion, double>? _religionMap;
+
+   public override int GetColorForLocation(Location location) => CalculatePluralityCulture(location).Color.AsInt();
+
+   private static Religion CalculatePluralityCulture(Location location)
+   {
+      _religionMap ??= new(8);
+      _religionMap.Clear();
+      foreach (var pop in location.Pops)
+      {
+         // Using CollectionsMarshal (Available in .NET 5+) avoids double-hashing (Contains + Add)
+         ref var currentWeight = ref CollectionsMarshal.GetValueRefOrAddDefault(_religionMap, pop.Religion, out _);
+         currentWeight += pop.Size;
+      }
+
+      var bestReligion = Religion.Empty;
+      var maxWeight = -1.0;
+
+      foreach (var kvp in _religionMap)
+         if (kvp.Value > maxWeight)
+         {
+            maxWeight = kvp.Value;
+            bestReligion = kvp.Key;
+         }
+
+      return bestReligion;
+   }
+
+   public override string[] GetTooltip(Location location)
+   {
+      var culture = CalculatePluralityCulture(location);
+      return [$"Plurality Religion: {culture.UniqueId}"];
+   }
+
+   public override string? GetLocationText(Location location) => null;
+
+   public override object?[]? GetVisualObject(Location location) => null;
+
+   public override void OnActivateMode()
+   {
+   }
+
+   public override void OnDeactivateMode()
+   {
+   }
+}
