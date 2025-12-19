f9ac6cc8f3b652513127063f580fcd6609975312
Minnator <info@minnator.com>
2025-12-19 15:38:01 +0100

    Started UI for Renamer window

---
+++ src/Arcanum.UI/Components/Windows/DebugWindows/Renamer.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Arcanum.UI.Components.Windows.DebugWindows;
+
+public partial class Renamer : Window
+{
+   public Renamer()
+   {
+      InitializeComponent();
+   }
+}
