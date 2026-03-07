8668c5770d4c626ea57235278bd8489083d52c94
Minnator <info@minnator.com>
2026-03-07 23:15:31 +0100

    Help window can be opened for any feature statically

--- src/Arcanum.UI/Components/Windows/HelpWindow/HelpWindow.xaml.cs
+++ src/Arcanum.UI/Components/Windows/HelpWindow/HelpWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Windows.Controls;
 using Arcanum.Core.CoreSystems.IO;
 using Arcanum.Core.GlobalStates;
+using Arcanum.UI.AppFeatures;
 using Arcanum.UI.Components.Windows.HelpWindow.ViewModels;
 
 namespace Arcanum.UI.Components.Windows.HelpWindow;
@@ -24,5 +25,13 @@ public partial class HelpWindow
    private void HelpWindow_OnClosing(object? sender, CancelEventArgs e)
    {
       JsonProcessor.Serialize(Path.Combine(IO.GetArcanumDataPath, Config.CONFIG_FILE_NAME), Config.Settings);
+   }
+
+   public static void OpenForFeature(IAppFeature feature)
+   {
+      HelpWindow window = new();
+      if (window.DataContext is HelpWindowViewModel vm)
+         vm.ActivateFeatureTabFor(feature);
+      window.ShowDialog();
    }
 }
