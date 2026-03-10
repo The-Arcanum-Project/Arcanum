7148a1640de01ad20fbb74a06a93dd64d8aff5e9
Minnator <info@minnator.com>
2026-03-10 01:31:12 +0100

    Fixed Institution changes not being saved.

--- src/Arcanum.Core/CoreSystems/History/Commands/BulkCollectionModificationCommand.cs
+++ src/Arcanum.Core/CoreSystems/History/Commands/BulkCollectionModificationCommand.cs
@@ -1,20 +1,18 @@
 ﻿using System.Diagnostics;
-using Arcanum.Core.CoreSystems.EventDistribution;
+using System.Runtime.CompilerServices;
 using Arcanum.Core.GameObjects.BaseTypes;
 
 namespace Arcanum.Core.CoreSystems.History.Commands;
 
-public class BulkCollectionModificationCommand : ICommand
+public class BulkCollectionModificationCommand : Eu5ObjectCommand
 {
    private readonly IEu5Object[] _targets;
-   private readonly Enum _attribute;
    private readonly object[] _values;
    private bool _add;
 
-   public BulkCollectionModificationCommand(IEu5Object[] targets, Enum attribute, object[] values, bool add)
+   public BulkCollectionModificationCommand(IEu5Object[] targets, Enum attribute, object[] values, bool add) : base(targets, attribute)
    {
       _targets = targets;
-      _attribute = attribute;
       _values = values;
       _add = add;
 
@@ -22,7 +20,7 @@ public class BulkCollectionModificationCommand : ICommand
       if (_targets.Length > 0)
       {
          Debug.Assert(_targets.Length == values.Length);
-         Debug.Assert(targets[0].GetAllProperties().Contains(_attribute));
+         Debug.Assert(targets[0].GetAllProperties().Contains(Attribute));
          Debug.Assert(targets[0].GetNxItemType(attribute) == values[0].GetType());
       }
 #endif
@@ -30,35 +28,36 @@ public class BulkCollectionModificationCommand : ICommand
       Execute();
    }
 
-   public void Execute()
+   public sealed override void Execute()
    {
       if (_add)
          for (var i = 0; i < _targets.Length; i++)
-            _targets[i]._addToCollection(_attribute, _values[i]);
+            _targets[i]._addToCollection(Attribute!, _values[i]);
       else
          for (var i = 0; i < _targets.Length; i++)
-            _targets[i]._removeFromCollection(_attribute, _values[i]);
+            _targets[i]._removeFromCollection(Attribute!, _values[i]);
 
-      EventDistributor.RegisterChanges(_values[0].GetType(), _attribute, _targets);
+      // base.Execute();
    }
 
-   public void Undo()
+   public override void Undo()
    {
       _add = !_add;
       Execute();
       _add = !_add;
    }
 
-   public void Redo()
+   public override void Redo()
    {
       Execute();
    }
 
-   public List<int> GetTargetHash() => _targets.Select(x => x.GetHashCode()).ToList();
+   public override IEu5Object[] GetTargets() => _targets;
 
-   public string GetDescription
-      => $"{(_add ? "Add" : "Remove")} {_values.Length} items to/from {_attribute} on {_targets.Length} targets of type {_targets[0].GetType().Name}";
+   public override string GetDescription
+      => $"{(_add ? "Add" : "Remove")} {_values.Length} items to/from {Attribute} on {_targets.Length} targets of type {_targets[0].GetType().Name}";
 
-   public string GetDebugInformation(int indent)
-      => $"{new string(' ', indent)}BulkCollectionModificationCommand: Attribute={_attribute}, Add={_add}, Targets={string.Join(", ", _targets.Select(t => t.GetHashCode()))}, Values={string.Join(", ", _values)}";
+   public override bool Equals(object? obj) => ReferenceEquals(this, obj);
+
+   public override int GetHashCode() => RuntimeHelpers.GetHashCode(this);
 }
