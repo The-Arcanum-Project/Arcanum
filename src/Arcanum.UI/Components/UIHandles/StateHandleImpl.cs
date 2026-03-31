a756b99b9be752ad0bbc8eee4f0d401eb1fcb292
Minnator <info@minnator.com>
2026-03-31 23:21:37 +0200

    Fixed debug access not being in #if DEBUG

--- src/Arcanum.UI/Components/UIHandles/StateHandleImpl.cs
+++ src/Arcanum.UI/Components/UIHandles/StateHandleImpl.cs
@@ -1,6 +1,10 @@
-﻿using Arcanum.Core.GlobalStates;
+﻿#region
+
+using Arcanum.Core.GlobalStates;
 using Arcanum.UI.Documentation;
 using Common.UI.State;
+
+#endregion
 
 namespace Arcanum.UI.Components.UIHandles;
 
@@ -8,8 +12,10 @@ public class StateHandleImpl : IStateHandle
 {
    public void ApplicationLoadResources()
    {
+#if DEBUG
       DocuPathResolver.LoadDocumentation(DebugConfig.Settings.UseExternalDocumentation,
                                          true,
                                          DebugConfig.Settings.ExternalDocumentationPath);
+#endif
    }
 }
