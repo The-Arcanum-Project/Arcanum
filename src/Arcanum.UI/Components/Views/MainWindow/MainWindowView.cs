18cf0a78f62a8b9dd1d08b8d278d0a66ddfc7123
minnator <info@minnator.de>
2026-09-18 13:48:41 +0200

    Fix

--- src/Arcanum.UI/Components/Views/MainWindow/MainWindowView.cs
+++ src/Arcanum.UI/Components/Views/MainWindow/MainWindowView.cs
@@ -10,7 +10,6 @@ namespace Arcanum.UI.Components.Views.MainWindow;
 public class MainWindowView : ObservableObject, IAppFeatureProvider
 {
    public FeatureId FeatureId => FeatureIds.Editor.MainWindow;
-   public AppFeature FeatureMetadata { get; } = EditorFeatures.MainWindowFeature;
 
    public EasterEgg2026 EasterEgg2026 { get; } = new();
 }
